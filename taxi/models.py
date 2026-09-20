a19418a820a1b6e94b8e8d5578663b0594c2faa7
CemXokenc <mohikancem@gmail.com>
2026-09-20 16:09:20 +0300

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -24,5 +24,5 @@ class Car(models.Model):
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
 
-    def __str__(self) -> type[Car]:
+    def __str__(self) -> str:
         return self.model
